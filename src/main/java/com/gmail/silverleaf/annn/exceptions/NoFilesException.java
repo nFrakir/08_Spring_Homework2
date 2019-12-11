17a74fce8a6028aabc960d11e13112294ced4d9a
Ann Seliverstova <n.frakir@gmail.com>
2019-12-11 21:45:48 +0200

    Initial commit.

---
+++ src/main/java/com/gmail/silverleaf/annn/exceptions/NoFilesException.java
@@ -0,0 +1,8 @@
+package com.gmail.silverleaf.annn.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+//@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.BAD_GATEWAY)
+public class NoFilesException extends RuntimeException {}
